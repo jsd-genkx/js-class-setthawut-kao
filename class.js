c38d33dbde1adcedf7c88fdd328da9819cb1e6f4
setthawut-kao <setthawut.kao@gmail.com>
2025-07-21 17:53:01 +0700

    js-class

--- class.js
+++ class.js
@@ -11,6 +11,13 @@
 // - In constructor, call super(message) and set this.name = "ValidationError"
 // ✏️ YOUR CODE HERE
 
+class ValidationError extends Error {
+    constructor(message){
+      super(message);
+      this.name = "ValidationError";
+    }
+}
+
 // ✅ STEP 2: Create a Product class
 // Requirements:
 // - Private field: #price
@@ -26,15 +33,66 @@
 // - static validateName(name): throw ValidationError if name is empty or shorter than 2 characters
 // ✏️ YOUR CODE HERE
 
+class Product {
+	#price; // private field
+
+	constructor(name, price, manufacturedDate) {
+		Product.validateName(name);
+		this.name = name;
+		this.price = price; // use setter
+		this.manufacturedDate = manufacturedDate;
+	}
+
+	get price() {
+		return this.#price;
+	}
+	set price(value) {
+		if (value < 0) throw new ValidationError("Price cannot be negative");
+		this.#price = value;
+	}
+
+	getInfo() {
+		return `${this.name} costs $${this.#price}`;
+	}
+
+	applyDiscount(percent) {
+		const discount = Math.floor((this.#price * percent) / 100);
+		this.#price -= discount;
+	}
+
+	getAge() {
+		const days = Math.floor(
+			(Date.now() - this.manufacturedDate) / (1000 * 60 * 60 * 24)
+		);
+		return days;
+	}
+
+	static validateName(name) {
+		if (!name || name.length < 2) {
+			throw new ValidationError("Name must be at least 2 characters");
+		}
+	}
+}
+
 // ✅ STEP 3: Create subclasses LandProduct and SeaProduct
 // - both extend Product
 // - override getInfo() to prefix "[Land]" or "[Sea]" before the parent's getInfo()
 // ✏️ YOUR CODE HERE
+class LandProduct extends Product {
+	getInfo() {
+		return `[Land] ${this.name} costs $${this.price}`;
+	}
+}
+class SeaProduct extends Product {
+	getInfo() {
+		return `[Sea] ${this.name} costs $${this.price}`;
+	}
+}
+
 
 // ✅ STEP 4: Test your code
 // After you finish implementing, uncomment the code below and run to see if it works.
 
-/*
 try {
   const apple = new LandProduct("Apple", 100, new Date(2025, 6, 1));
   console.log(apple.getInfo()); // [Land] Apple costs $100
@@ -56,7 +114,6 @@ try {
     console.error("Error:", err.message);
   }
 }
-*/
 
 // ===============================
 // ✅ BONUS (Optional)
